01348acb746d406726598fb7a06c628509cb2bfb
taphan <trucphan307@gmail.com>
2016-10-27 12:50:49 +0200

    Color v2

--- behavior.py
+++ behavior.py
@@ -33,22 +33,41 @@ class Color(Behavior):
         super(Color, self).__init__()
         self.priority = 1
         self.line_status = self.bbcon.line_status
+        self.sensor = sensob.CameraSensob()
+        self.sensobs.append(self.sensor)
 
     def consider_deactivation(self):
         if self.active_flag: # Forsørg at testen skjer bare når den er på
-
+            self.active_flag = False
 
     def consider_activation(self):
         # Aktiveres kun når enden av linjen er funnet
         if self.line_status == 2:
             self.active_flag = True
 
-
     def update(self):
-        pass
+        if not self.active_flag:
+            self.sensor.reset()
+        else:
+            self.sense_and_act()
+            self.weight = self.priority*self.match_degree
 
     def sense_and_act(self):
-        pass
+        """
+        Rød: Snu venstre
+        Blå: Snu høyre
+        Grønn: Snu 180 grader og stopp
+        """
+        self.sensor.update()
+        color = self.sensor.get_color()
+        if color == 'red':
+            self.motor_recs = ('L', 90)
+        elif color == 'blue':
+            self.motor_recs = ('R', 90)
+        else:
+            self.motor_recs = ('L', 180)
+            self.halt_request = True # Stopper hele testen dersom
+
 
 class FollowLine(Behavior):
 
