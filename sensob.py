7a6e5c7bf6d7477906e102ba9d046a8d6bcb9c98
Sigrid L. Bjørndal <Sigrid L. Bjrndal>
2016-10-27 10:23:47 +0200

    Implementert Sensob's metoder i ReflectanceSensob

--- sensob.py
+++ sensob.py
@@ -13,7 +13,6 @@ class Sensob():
 class ReflectanceSensOb(Sensob):
 
     def __init__(self):
-<<<<<<< HEAD
         # bruker min og max for å kalibrere. Kan endres til auto_calibrate
         self.sensor = reflectance_sensors.ReflectanceSensors(auto_calibrate=False, min_reading=100, max_reading=1000)
         self.get_value()
@@ -27,6 +26,3 @@ class ReflectanceSensOb(Sensob):
     def reset(self):
         self.sensor.reset()
 
-=======
-        pass
->>>>>>> 8bd62f43ea012c869422fc0c361a5e503f685409
