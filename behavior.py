97420b33872baf14f775b133f7d7abd6046a6076
Sigrid <sigrid.bjorndal@hotmail.com>
2016-10-27 12:16:02 +0200

    Oppdatert FollowLine

--- behavior.py
+++ behavior.py
@@ -18,7 +18,7 @@ class Behavior():
     def consider_deactivation(self):
         raise NotImplementedError
 
-    def consider_deactivation(self):
+    def consider_activation(self):
         raise NotImplementedError
 
     def update(self):
@@ -26,11 +26,6 @@ class Behavior():
 
     def sense_and_act(self):
         raise NotImplementedError
-
-class FollowLine(Behavior):
-
-    def __init__(self):
-        super(FollowLine, self).__init__()
 
 class Color(Behavior):
 
@@ -53,3 +48,41 @@ class Color(Behavior):
 
     def sense_and_act(self):
         pass
+
+class FollowLine(Behavior):
+
+    def __init__(self):
+        super(FollowLine, self).__init__()
+        sensor = sensob.ReflectanceSensOb()
+        self.sensobs.append(sensor)
+        self.active_flag = True
+        self.priority = 0.7
+        self.steps_line_followed = 0
+        self.line_found_flag = False
+
+    def consider_deactivation(self):
+        line_status = 1
+        if line_status == 2:
+            self.active_flag = False
+            return True
+
+    def consider_activation(self):
+        line_status = 0
+        if line_status == 0:
+            self.active_flag = True
+            return True
+        elif line_status == 1:
+            self.active_flag = True
+            return True
+
+    def sense_and_act(self):
+        if self.line_found_flag:
+            self.follow_line()
+        else:
+            self.find_line()
+
+    def follow_line(self):
+        pass
+
+    def find_line(self):
+        sensor = self.sensobs[0]
