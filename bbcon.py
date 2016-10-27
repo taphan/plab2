c0e1fdd087855a5dac8cfc78003b2e9634e20f3e
taphan <trucphan307@gmail.com>
2016-10-27 13:01:28 +0200

    bbcon og arbitrator v1

--- bbcon.py
+++ bbcon.py
@@ -1,3 +1,5 @@
+import arbitrator
+
 class BBCON():
     def __init__(self):
         """
@@ -6,3 +8,28 @@ class BBCON():
         2: Line end found
         """
         self.line_status = 0 # Blir lest av Color og Wander behaviors for å sjekke om de kan bli aktivert
+        self.timestep = 0
+
+        self.behaviors = []
+        self.active_behaviors = []
+        self.sensobs = []
+        self.motobs = []
+        self.arbitrator = arbitrator.Arbitrator()
+
+    def add_behavior(self, behavior):
+        # Append a newly-created behavior onto the behaviors list.
+        self.behaviors.append(behavior)
+
+    def add_sensob(self, sensob):
+        # Append a newly-created sensob onto the sensobs list.
+        self.sensobs.append(sensob)
+
+    def activate_behavior(self, behavior):
+        # Add an existing behavior onto the active-behaviors list.
+        if behavior in self.behaviors:
+            self.active_behaviors.append(behavior)
+
+    def deactive_behavior(self, behavior):
+        # Remove an existing behavior from the active behaviors list.
+        if behavior in self.behaviors:
+            self.active_behaviors.remove(behavior)
